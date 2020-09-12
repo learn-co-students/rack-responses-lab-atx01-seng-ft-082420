f0645e4b46718650ec8277c5e3d1a3a806202a41
jlonetree <jlonetree@gmail.com>
2020-09-12 14:19:46 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     def call(env)
         resp = Rack::Response.new
 
-        (Time.now.to_i < 12) ?  ["<em>Good Morning!</em>"] : ["<strong>Good Afternoon!</strong>"]
+        (Time.now.to_i < 12) ?  ["<em>Good Morning!</em>"] : ["<em>Good Afternoon!</em>"]
 
         resp.write "Good Morning!"
         resp.write "Good Afternoon!"
